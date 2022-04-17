c07c044bc447796c40be88168af6dfed4c41469a
Giovanni Claro <gio.claro316@gmail.com>
2022-04-17 01:22:14 -0400

    added prompts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,42 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var enemyName = "Megan";
+var enemyHealth = "50";
+var enemyAttack = "12";
+
+console.log(playerName, playerAttack, playerHealth);
+
+var fight = function () {
+    window.alert("Welcome to Robot Gladiators!")
+
+    //Subract enemyHealth from playerAttack
+    enemyHealth = enemyHealth-playerAttack;
+    //log message
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+    //check enemy health
+    if(enemyHealth <=0) {
+        window.alert(enemyName+ " has died!");
+    }
+    else{
+        window.alert(enemyName+ " still has "+enemyHealth+ " health left.");
+    }
+    //Subtract playerHealth from enemyAttack
+    playerHealth = playerHealth-enemyAttack;
+    //log message
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      );
+    //check player health
+    if (playerHealth <=0) {
+        window.alert(playerName+ " has died");
+    }
+    else {
+        window.alert(playerName+ " still has "+ playerHealth + " health left.");
+    }
+
+};
+
+fight();
