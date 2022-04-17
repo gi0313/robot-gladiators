c92419b8638ef86f03cf2530a9cdcaf0d6420c2f
Giovanni Claro <gio.claro316@gmail.com>
2022-04-17 02:09:07 -0400

    initial setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,18 +4,21 @@ var playerAttack = 10;
 var enemyName = "Megan";
 var enemyHealth = "50";
 var enemyAttack = "12";
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
 var fight = function () {
     window.alert("Welcome to Robot Gladiators!")
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    if(promptFight === "fight" || promptFight === "FIGHT") {
 
     //Subract enemyHealth from playerAttack
     enemyHealth = enemyHealth-playerAttack;
     //log message
     console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        );
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
     //check enemy health
     if(enemyHealth <=0) {
         window.alert(enemyName+ " has died!");
@@ -27,8 +30,7 @@ var fight = function () {
     playerHealth = playerHealth-enemyAttack;
     //log message
     console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-      );
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
     //check player health
     if (playerHealth <=0) {
         window.alert(playerName+ " has died");
@@ -36,7 +38,23 @@ var fight = function () {
     else {
         window.alert(playerName+ " still has "+ playerHealth + " health left.");
     }
-
-};
+    //If player skipped
+    } else if (promptFight === "skip" || promptFight ==="SKIP") {
+        //confirm skip
+        var confirmSkip = window.confirm("Are you sure you want to quit?");
+        //if yes
+        if(confirmSkip) {
+            window.alert(playerName+ " has chosen to skip the fight!");
+            //subrtact money
+            playerMoney = -2;
+        }
+        //if no
+        else {
+            fight();
+        }
+    } else {
+        window.alert("You need to cheoose a valid otion. Try again!");
+    }
+}
 
 fight();
